8931508ff47c3abf610e21d78de352c28c0e9832
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-05 23:54:22 +0300

    load vk

---
+++ vk.py
@@ -0,0 +1,38 @@
+import json
+
+punct = ['.', ',', '!', '?', ')', '(']
+
+
+def take_from_html():
+    with open('chat.html', 'r', encoding='windows-1251') as f:
+        text = f.read()
+    f.close()
+    data_string = ''
+    position = text.find('<div class="im-mess--text wall_module _im_log_body">')
+    while text and position != -1:
+        text = text[position+52:]
+        check_pose = text.find('<')
+        position = text.find('</div>')
+        if position == check_pose:
+            word = text[:position].lower()
+            for c in punct:
+                word = word.replace(c, '')
+            data_string += text[:position] + ' '
+        position = text.find('<div class="im-mess--text wall_module _im_log_body">')
+
+    return data_string
+
+
+def take_from_json():
+    data_string = ''
+    with open('dialog.json', 'r', encoding='utf-8-sig') as f:
+        data = json.load(f)
+    f.close()
+    for msg in data['data']:
+        if msg.get('text') is not None:
+            if type(msg['text']) == str:
+                word = msg['text'].lower()
+                for c in punct:
+                    word = word.replace(c, '').lower()
+                data_string += word + ' '
+    return data_string
