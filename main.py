28ecc7c8931d4a2db1e48597ffe71bc80ec19263
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-07 11:36:07 +0300

    comments added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# подключение библиотек
 from wordcloud import WordCloud
 from stop_words import get_stop_words
+# и файла vk.py
 import vk
-
+# стоп слов
 stopwords = get_stop_words("ru")
-
+# настройка картинки
 wc = WordCloud(
     # цвет фона (можно менять)
     background_color='white',
@@ -16,17 +18,23 @@ wc = WordCloud(
     # python anywhere справляется только с 80ю, компьютер легко обрабатывает больше 1000
     max_words=1000
 )
-
+# ввод имени файла
 print('Введитe имя файла(json или html):')
 name = input()
+# проверка расширения
 dot_place = name.find('.')
 if name[dot_place+1:] == 'html':
+    # генерация картинки
     wc.generate(vk.take_from_html(name))
+    # сохранение картинки
     wc.to_file('word_cloud.png')
 elif name[dot_place+1:] == 'json':
+    # генерация картинки
     wc.generate(vk.take_from_json(name))
+    # сохранение картинки
     wc.to_file('word_cloud.png')
+# обработка ошибки расширения
 else:
-    print("пожалуйста, выбирайте файл с расширением json или html")
+    print("Пожалуйста, выбирайте файл с расширением json или html")
 
 
--- vk.py
+++ vk.py
@@ -1,38 +1,61 @@
+# подключение библиотеки
 import json
-
+# список знаков препинания
 punct = ['.', ',', '!', '?', ')', '(']
 
 
+# функция подготовки html файла
+# принимает имя файла
+# возвращает строку сообщений
 def take_from_html(name):
+    # открытие файла
     with open(name, 'r', encoding='windows-1251') as f:
         text = f.read()
     f.close()
+    # создание строки результата
     data_string = ''
+    # поиск html тэга, заключающего в себе текст сообщения
     position = text.find('<div class="im-mess--text wall_module _im_log_body">')
+    # проход по всему файлу
     while text and position != -1:
+        # срез текста файла до начала текста ближайшего сообщения
         text = text[position+52:]
+        # проверка, что в тэг (строка 18) заключен только текст сообщения
         check_pose = text.find('<')
         position = text.find('</div>')
         if position == check_pose:
+            # срез текста сообщения, перевод в нижний регистр
             word = text[:position].lower()
+            # исключение пунктуации
             for c in punct:
                 word = word.replace(c, '')
+            # добавление сообщения в строку
             data_string += text[:position] + ' '
+        # поиск html следующего тэга, заключающего в себе текст сообщения
         position = text.find('<div class="im-mess--text wall_module _im_log_body">')
-
     return data_string
 
 
+# функция подготовки json файла
+# принимает имя файла
+# возвращает строку сообщений
 def take_from_json(name):
-    data_string = ''
+    # открытие файла
     with open(name, 'r', encoding='utf-8-sig') as f:
         data = json.load(f)
     f.close()
+    # создание строки результата
+    data_string = ''
+    # перебор сообщений
     for msg in data['data']:
+        # проверка, что текст сообщения есть
         if msg.get('text') is not None:
             if type(msg['text']) == str:
+                # перевод всего в нижний регистр
                 word = msg['text'].lower()
+                # исключение пунктуации
                 for c in punct:
                     word = word.replace(c, '').lower()
+                # добавление сообщения в строку
                 data_string += word + ' '
     return data_string
